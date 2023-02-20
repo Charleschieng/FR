b099fcba4415065471fab8b1bdedd02f6ffa3d48
Communist Bee <50063234+Charleschieng@users.noreply.github.com>
2023-02-20 08:18:14 +0000

    Encoded Sans

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("SansFight","SansFight.mp4",.3,.3,10)
+    encoder.encode("SansFight","SansFight.mp4",.225,.225,10)
 
 if __name__== "__main__":
     main()
