825bec32f1e9ffa66d45b3d3c90b41a31621ac30
Communist Bee <50063234+Charleschieng@users.noreply.github.com>
2023-02-20 07:10:04 +0000

    Yes.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("SansFight","SansFight.mp4",.1,.1,10)
+    encoder.encode("SansFight","SansFight.mp4",.3,.3,10)
 
 if __name__== "__main__":
     main()
